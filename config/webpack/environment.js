d8b4ffcfc6e05efed416503837e199e3d7fb38e9
apollo <apollo@Air-di-apollo.lan>
2019-09-11 09:46:09 +0200

    T1_5: Create Static Page with fix

--- config/webpack/environment.js
+++ config/webpack/environment.js
@@ -1,10 +1,11 @@
 const { environment } = require('@rails/webpacker')
 
-const webpack = require['webpack']
-environment.plugin.append('Provide',
+const webpack = require('webpack')
+environment.plugins.append('Provide',
     new webpack.ProvidePlugin({
         $: 'jquery/src/jquery',
         jQuery: 'jquery/src/jquery'
     })
 )
+
 module.exports = environment
