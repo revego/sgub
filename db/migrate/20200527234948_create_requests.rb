83cf12e32695a0c13e88ecc285171e9ee3494308
Code4you <40631055+code-4you@users.noreply.github.com>
2020-05-31 16:31:46 +0200

    2+Task1

---
+++ db/migrate/20200527234948_create_requests.rb
@@ -0,0 +1,13 @@
+class CreateRequests < ActiveRecord::Migration[6.0]
+  def change
+    create_table :requests do |t|
+      t.text :description
+      t.integer :budget
+      t.integer :delivery
+      t.references :user, null: false, foreign_key: true
+      t.references :category, null: false, foreign_key: true
+
+      t.timestamps
+    end
+  end
+end
