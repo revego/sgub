95ec11c636a74d13080453d1a811b93b90de6e84
Code4you <40631055+code-4you@users.noreply.github.com>
2020-06-01 08:34:26 +0200

    2-T2

--- app/controllers/requests_controller.rb
+++ app/controllers/requests_controller.rb
@@ -1,7 +1,7 @@
 class RequestsController < ApplicationController
   before_action :authenticate_user!
-  before_action :set_request, except: [:new, :create, :index, :list]
-  before_action :is_authorized, only: [:edit, :update, :destroy]
+  before_action :set_request, except: [:new, :create, :index, :list, :my_offers]
+  before_action :is_authorized, only: [:edit, :update, :destroy, :offers]
   before_action :set_categories, only: [:new, :edit, :list]
   def index
     @requests = current_user.requests
@@ -47,6 +47,14 @@ class RequestsController < ApplicationController
     else
      @requests = Request.all 
     end
+  end
+
+  def offers
+    @offers = @request.offers
+  end
+
+  def my_offers
+    @offers = current_user.offers
   end
 
   private
