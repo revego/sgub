0892227a07fa4399acb1ca751d1b976abea525e8
Gogs <gogs@fake.local>
2020-07-07 17:42:49 +0200

    fix develop:good

--- config/initializers/stripe.rb
+++ config/initializers/stripe.rb
@@ -1,2 +1,8 @@
 Stripe.api_key = Rails.application.credentials[Rails.env.to_sym][:stripe][:STRIPE_SECRET_KEY]
 
+#Rails.configuration.stripe = {
+#:publishable_key => ENV['STRIPE_PUBLISHABLE_KEY'],
+#:secret_key => ENV['STRIPE_SECRET_KEY']
+#}
+
+#Stripe.api_key = Rails.configuration.stripe[:secret_key]
